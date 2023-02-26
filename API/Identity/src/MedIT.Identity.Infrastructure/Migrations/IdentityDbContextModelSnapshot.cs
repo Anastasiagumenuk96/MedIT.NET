4a9723ca741a0f3e62c7da24642c5ee48a2a426b
Nastinka <Anastasiagumenuk96@gmail.com>
2023-02-26 13:19:02 +0200

    removed redundant usings

--- API/Identity/src/MedIT.Identity.Infrastructure/Migrations/IdentityDbContextModelSnapshot.cs
+++ API/Identity/src/MedIT.Identity.Infrastructure/Migrations/IdentityDbContextModelSnapshot.cs
@@ -3,8 +3,6 @@ using System;
 using MedIT.Identity.Infrastructure.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
